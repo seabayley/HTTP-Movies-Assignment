56ea8e4ed7edda87aa93075c690e89cf3408b219
Christopher Bailey <seabayley@outlook.com>
2019-10-17 18:07:09 -0500

    Added some styling

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField'
+import Slider from '@material-ui/core/Slider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Button from '@material-ui/core/Button';
+
 import axios from 'axios'
 
 const UpdateMovie = props => {
     const [movie, setMovie] = useState(props.location.state.movie || {})
     const [metascore, setMetascore] = useState(0)
-    const [stars, setStars] = useState([])
+    const [stars, setStars] = useState(props.location.state.movie.stars)
 
     const handleTitleChange = e => {
         setMovie({ ...movie, title: e.target.value })
@@ -15,7 +24,7 @@ const UpdateMovie = props => {
     }
 
     const handleMetascoreChange = e => {
-        setMetascore(e.target.value)
+        setMovie({ ...movie, metascore: Number(e.target.textContent) })
     }
 
     const handleStarsChange = e => {
@@ -23,7 +32,6 @@ const UpdateMovie = props => {
     }
 
     const handleUpdateMovie = e => {
-        e.preventDefault()
         axios
             .put(`http://localhost:5000/api/movies/${movie.id}`, { ...movie })
             .then(response => {
@@ -34,27 +42,55 @@ const UpdateMovie = props => {
             })
     }
 
+    const useStyles = makeStyles(theme => ({
+        root: {
+            width: 300,
+            padding: '1rem',
+            margin: '1rem auto'
+        },
+        margin: {
+            height: theme.spacing(3),
+        },
+    }));
+
+    const classes = useStyles();
+
     return (
-        <div className="update-form">
-            <form onSubmit={(e) => handleUpdateMovie(e)}>
-                Title
-                <input
-                    type="text"
-                    name="title"
-                    value={movie.title}
-                    placeholder={movie.title}
-                    onChange={handleTitleChange}
-                />
-                Director
-                <input
-                    type="text"
-                    name="director"
-                    value={movie.director}
-                    placeholder={movie.director}
-                    onChange={handleDirectorChange}
-                />
-                <button>Update</button>
-            </form>
+        <div className={classes.root}>
+            <TextField
+                id="movie-title"
+                label="Title"
+                value={movie.title}
+                onChange={handleTitleChange}
+                margin="normal"
+                variant="outlined"
+            />
+            <TextField
+                id="director-name"
+                label="Director"
+                value={movie.director}
+                onChange={handleDirectorChange}
+                margin="normal"
+                variant="outlined"
+            />
+            <Typography id="metascore-slider">Metascore</Typography>
+            <Slider
+                defaultValue={50}
+                step={1}
+                marks
+                min={0}
+                max={100}
+                onChange={handleMetascoreChange}
+                valueLabelDisplay="auto"
+            />
+            <Typography id="stars-list">Movie Stars</Typography>
+            <List>
+                {stars.map(star => <ListItem><ListItemText>{star}</ListItemText></ListItem>)}
+            </List>
+            <Button variant='contained' color='primary'>Add Star</Button>
+            <Button variant='contained' color='secondary'>Remove Star</Button>
+            <Button variant='contained' color='primary' onClick={(e) => handleUpdateMovie(e)}>Update</Button>
+            <Button variant='contained' color='secondary'>Cancel</Button>
         </div>
     );
 };
